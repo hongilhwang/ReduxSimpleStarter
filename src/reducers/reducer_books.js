ee0d771bdbd9d78e0920cf0ea225325e50bdc4c3
steven.hwang <steven.hwang@kakaocorp.com>
2018-01-29 18:03:56 +0900

    리듀서를 생성하고, reducer 컨바인으로 루트 리듀서에 연결

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        { title : 'JavaScript' },
+        { title : 'React 깨부수기' },
+        { title : '회사에서 공부 하며 시간때우기...ㅠ.ㅠ' },
+        { title : '배운것에 대해 업무에 적용 시킬려면?' }
+    ];
+};
